1834d10cc20f9d57a8211e9f6153e91f33876995
Edgars Pavlovskis <epavlovskis01@qub.ac.uk>
2020-12-17 21:07:17 +0000

    Comments added - test commit

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -1,10 +1,12 @@
 package uk.ac.qub.eeecs.dave;
 
 public class Calculator {
+	//Add function
 	public int add(int a, int b) {
 		return a+b;
 	}
 	
+	//Sub function
 	public int sub(int a, int b) {
 		return a-b;
 	}
